8c74a36bdf4c3e1fafef82cb6b85477c4b643cc0
eoganz <jakeemettler@gmail.com>
2018-10-01 13:15:04 -0700

    Changed title on layout page

--- HomeTownZooProject/Controllers/HomeController.cs
+++ HomeTownZooProject/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace HomeTownZooProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "HomeTown Zoo";
+            ViewBag.Message = "HomeTown Zoo!";
 
             return View();
         }
